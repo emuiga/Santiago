4f0890b5c7596f0d33d1a2d31336dcbc1739e1d6
emuiga <muigastephen14@gmail.com>
2023-07-20 17:13:55 +0300

    featurechanges

--- admin/manage-categories.php
+++ admin/manage-categories.php
@@ -133,9 +133,9 @@ foreach($results as $result)
                                             <td class="center"><?php echo htmlentities($cnt);?></td>
                                             <td class="center"><?php echo htmlentities($result->CategoryName);?></td>
                                             <td class="center"><?php if($result->Status==1) {?>
-                                            <a href="#" class="btn btn-success btn-xs">Activate</a>
+                                            <a href="#" class="btn btn-success btn-xs">Active</a>
                                             <?php } else {?>
-                                            <a href="#" class="btn btn-danger btn-xs">Deactivate</a>
+                                            <a href="#" class="btn btn-danger btn-xs">Inactive</a>
                                             <?php } ?></td>
                                             <td class="center"><?php echo htmlentities($result->CreationDate);?></td>
                                             <td class="center"><?php echo htmlentities($result->UpdationDate);?></td>
